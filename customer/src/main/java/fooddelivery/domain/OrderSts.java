dcfd7b11f8e79bb86e89bcbda2bdb9fbcedd6aa5
fromzihee <78025432+fromzihee@users.noreply.github.com>
2022-11-22 13:03:07 +0900

    commit

---
+++ customer/src/main/java/fooddelivery/domain/OrderSts.java
@@ -0,0 +1,19 @@
+package fooddelivery.domain;
+
+import javax.persistence.*;
+import java.util.List;
+import java.util.Date;
+import lombok.Data;
+
+@Entity
+@Table(name="OrderSts_table")
+@Data
+public class OrderSts {
+
+        @Id
+        //@GeneratedValue(strategy=GenerationType.AUTO)
+        private Long id;
+        private String foodid;
+
+
+}
